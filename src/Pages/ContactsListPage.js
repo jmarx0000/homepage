add4053dfe6c93209310c668b88ee9b973051907
jmarx0000 <jmarx000@gmail.com>
2024-10-07 01:47:25 -0700

    formatted output for existing contacts

--- src/Pages/ContactsListPage.js
+++ src/Pages/ContactsListPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import ContactTile from '../Components/ContactTile';
 import { db } from '../Configuration/firebase'; 
 import { collection, getDocs } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import './ContactsListPage.css'; // Import the CSS file
 
 const ContactsList = () => {
     const [contacts, setContacts] = useState([]);
@@ -25,43 +25,42 @@ const ContactsList = () => {
 
     return (
       <div>
-      <div style={{ width: '200px', margin: '20px' }}>
-        <Link to="/">
-        <button className="button-primary">Back to Home</button>
-        </Link>
-      </div>
-      
-      <div className="contacts-list-page">
-        <table>
-        <thead>
-          <tr>
-          <th>Full Name</th>
-          <th>Firm Name</th>
-          <th>Position</th>
-          <th>Last Reach Out</th>
-          <th>Email</th>
-          <th>Phone Number</th>
-          <th>Notes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map(contact => (
-          <tr key={contact.id}>
-            <td>{contact.fullName}</td>
-            <td>{contact.firmName}</td>
-            <td>{contact.position}</td>
-            <td>{contact.lastReachOut}</td>
-            <td>{contact.email}</td>
-            <td>{contact.phoneNumber}</td>
-            <td>{contact.notes}</td>
-          </tr>
-          ))}
-        </tbody>
-        </table>
-      </div>
+        <div style={{ width: '200px', margin: '20px' }}>
+          <Link to="/">
+            <button className="button-primary">Back to Home</button>
+          </Link>
+        </div>
+        
+        <div className="contacts-list-container">
+          <table className="contacts-list-table">
+            <thead>
+              <tr>
+                <th>Full Name</th>
+                <th>Firm Name</th>
+                <th>Position</th>
+                <th>Last Reach Out</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {contacts.map(contact => (
+                <tr key={contact.id}>
+                  <td>{contact.fullName}</td>
+                  <td>{contact.firmName}</td>
+                  <td>{contact.position}</td>
+                  <td>{contact.lastReachOut}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.phoneNumber}</td>
+                  <td>{contact.notes}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
 };
 
 export default ContactsList;
-
